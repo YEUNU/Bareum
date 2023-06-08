693dfc7edb2feed3ccc6bdb8382e83748bf3b013
NowHyeok <wpgur4694@naver.com>
2023-06-08 23:39:12 +0900

    pwa

---
+++ front/vite.config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['/icons/*'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/android-chrome-512x512',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      } 
+    
+    }),
+    ],
+    server: {
+      host:'0.0.0.0',
+      proxy: {
+        '/api': { 
+          target: 'http://django:8000', 
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          secure: false,
+          ws: true
+        }
+      }
+    },
+})
