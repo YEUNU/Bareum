666ca397e38f255e0a6d4046b41d8e98d4094f6b
NowHyeok <wpgur4694@naver.com>
2023-06-12 15:49:15 +0900

    0612

---
+++ front/src/pages/Search-page/Search.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        검색페이지임
+    </div>
+    <router-link to="/search/detail"> 상세 검색하기</router-link>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
