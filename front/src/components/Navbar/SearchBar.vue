0ae36df926286bc1b158649abec63966ab2032c9
mskim <kms97531@gmail.com>
2023-06-14 10:50:26 +0900

    trim searchbar

--- front/src/components/Navbar/SearchBar.vue
+++ front/src/components/Navbar/SearchBar.vue
@@ -1,56 +1,60 @@
 <template>
     <div class="search">
-      <input type="text" :placeholder="placeholder" v-model="searchQuery" @keydown.enter="search">
-      <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png" @click="search">
-    </div>
-  </template>
-  
-  <script>
-  import { ref, defineProps } from 'vue';
-  
-  export default {
+        <input class="search-input" type="text" :placeholder="placeholder" v-model="searchQuery" @keydown.enter="search">
+    <img class="search-icon" src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png" @click="search">
+  </div>
+</template>
+
+<script>
+import { ref, defineProps } from 'vue';
+
+export default {
     props: {
-      placeholder: {
-        type: String,
-        default: '검색어 입력'
-      }
-    },
-    setup(props) {
-      const searchQuery = ref('');
-  
-      const search = () => {
-        // 검색 로직
-        console.log('검색어:', searchQuery.value);
-      };
-  
-      return {
-        searchQuery,
-        search
-      };
+        placeholder: {
+            type: String,
+      default: '검색어 입력'
     }
-  };
-  </script>
-  
-  <style>
-  .search {
+  },
+  setup(props) {
+        const searchQuery = ref('');
+    
+    const search = () => {
+            // 검색 로직
+      console.log('검색어:', searchQuery.value);
+    };
+    
+    return {
+            searchQuery,
+      search
+    };
+  }
+};
+</script>
+
+<style>
+.search {
     position: relative;
     width: 100%;
-  }
-  
-  input {
+    height: 100%;
+    display: flex;
+    align-items: center;
+}
+
+.search-input {
     width: 100%;
     border: 1px solid #bbb;
     border-radius: 8px;
     padding: 10px 12px;
     font-size: 14px;
-  }
-  
-  img {
-    position: absolute;
+    box-sizing : border-box;
+}
+
+.search-icon {
+  position: absolute;
     width: 17px;
-    top: 10px;
-    right: 12px;
+        right: 12px;
     margin: 0;
-  }
-  </style>
-  
+   	z-index: 1;
+}
+
+</style>
