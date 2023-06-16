82baed413ae0b21131a6ee1156e1ef83684e2554
junhyeok <natenim@naver.com>
2023-06-16 17:41:38 +0900

    update

--- front/src/stores.js
+++ front/src/stores.js
@@ -37,51 +37,6 @@ export const useStore = defineStore('app', {
 
 
 
-//현재 로그인한 유저의 정보
-export const useUserInfo = defineStore('userInfo',{
-  state : ()=>({
-    isLoggedIn:false,
-    userId:"",
-    name:"",
-    age:null,
-    sex:null,
-  }),
-  getters:{
-    getIsLoggedIn(state){
-      return this.isLoggedIn
-    },
-    getUserInfo(state){
-      return state.userId, state.name, state.age, state.sex
-    }
-  },
-  actions:{
-    userLogin(userId,name){
-      this.name = name;
-      this.userId = userId;
-      this.isLoggedIn = true;
-    },
-    userLogout() {
-      this.isLoggedIn = false;
-      this.isLoggedIn=false;
-      this.userId="";
-      this.name="";
-      this.age=null;
-      this.sex=null;
-    },
-  }
-})
-
-export const userNutraceuticals = defineStore('nutraceuticals',{
-  state:()=>({
-    name:"",
-  }),
-  getters:{
-
-  },
-  actions:{
-    
-  }
-})
 
 //  현재 로그인한 유저와 정보
 export const useUserInfo = defineStore('userInfo',{
