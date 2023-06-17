4207dc313d51b264b6cde3328783a0c01ccb0963
seungmin1021 <wp050312@gmail.com>
2023-06-17 18:13:19 +0900

    scroll

--- front/src/pages/Community-page/Post.vue
+++ front/src/pages/Community-page/Post.vue
@@ -5,9 +5,9 @@
           <div class="row">
             <div class="col-6">
               <div class="card-body">
-                <p class="card-text">{{ post.title }}</p>
-                <p class="card-text">{{ post.author }}</p>
-                <p class="card-text">{{ post.extra }}</p>
+                <p class="card-text">{{ post.post_date }}</p>
+                <p class="card-text">제목 : {{ post.post_title }}</p>
+                <p class="card-text">내용 : {{ post.post_contents }}</p>
               </div>
             </div>
             <div class="col-6">
@@ -16,6 +16,7 @@
           </div>
         </router-link>
       </div>
+      <div ref="loader" class="loader"></div>
       <router-link
         to="/community/write"
         tag="button"
@@ -27,53 +28,60 @@
 </template>
 
 <script>
-  import axios from 'axios';
-  import { ref, onMounted } from 'vue';
-  
-  export default {
-    setup() {
-      const posts = ref([]);
-      const page = ref(1);
-      const per_page = 10;
-      const postTotalPages = ref(1);
-  
-      async function fetchPosts() {
-        if (page.value <= postTotalPages.value) {
-          try {
-            const response = await axios.get(`api/community/list?page=${page.value}`);
-            posts.value.push(...response.data.results);
-            postTotalPages.value = Math.ceil(response.data.count / per_page);
-            page.value += 1;
-          } catch (err) {
-            console.error('err:', err);
-          }
-        }
-      }
-  
-      onMounted(() => fetchPosts());
-      return {
-        posts,
-        fetchPosts
-      }
-    },
-    directives: {
-      infiniteScroll: {
-        mounted(el, binding) {
-          const { value } = binding;
-          window.addEventListener('scroll', async () => {
-            let scrollHeight = document.documentElement.scrollHeight;
-            let scrollTop = document.documentElement.scrollTop;
-            let clientHeight = document.documentElement.clientHeight;
-  
-            if (scrollTop + clientHeight >= scrollHeight) {
-              await value();
-            }
-          });
+import axios from 'axios';
+import { ref, onMounted, onUnmounted } from 'vue';
+
+export default {
+  setup() {
+    const posts = ref([]);
+    const page = ref(1);
+    const per_page = 10;
+    const postTotalPages = ref(1);
+    const loader = ref(null);
+
+    async function fetchPosts() {
+      if (page.value <= postTotalPages.value) {
+        try {
+          const response = await axios.get(`api/community/list?page=${page.value}`);
+          posts.value.push(...response.data.results);
+          postTotalPages.value = Math.ceil(response.data.count / per_page);
+          page.value += 1;
+        } catch (err) {
+          console.error('err:', err);
         }
       }
     }
-  };
-  </script>
+
+    let observer;
+    
+    onMounted(async () => {
+      await fetchPosts();
+
+      observer = new IntersectionObserver(async (entries, observer) => {
+        if (entries[0].isIntersecting) {
+          await fetchPosts();
+        }
+      });
+
+      if (loader.value) {
+        observer.observe(loader.value);
+      }
+    });
+
+    onUnmounted(() => {
+      if (loader.value) {
+        observer.unobserve(loader.value);
+      }
+    });
+
+    return {
+      posts,
+      loader,
+    };
+  },
+};
+</script>
+
 
   <style lang="css">
   .fixed-button {
