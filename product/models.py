40382e50a529129536bbaeaf11c9bee09eaacffb
dntjr970 <dntjr970@gmail.com>
2023-06-20 12:24:22 +0900

    마이그레이션 수정 및 db 데이터 저장

--- product/models.py
+++ product/models.py
@@ -8,37 +8,62 @@ class Company(models.Model):
         managed = True
         db_table = 'company'
 
-
-
-class Ingredient(models.Model):
-    ingredient_id = models.BigAutoField(primary_key=True)
-
-    class Meta:
-        managed = True
-        db_table = 'ingredient'
-
 class Nutraceuticals(models.Model):
     nutraceuticals_id = models.BigAutoField(primary_key=True)
-    nutraceuticals_name = models.CharField(max_length=100)
-    nutraceuticals_categroy = models.CharField(max_length=1)
-    nutraceuticals_rating = models.IntegerField()
-    nutraceuticals_effects = models.CharField(max_length=100)
-    nutraceuticals_side_effects = models.CharField(max_length=100)
-    nutraceuticals_company = models.CharField(max_length=100)
-    company = models.ForeignKey(Company, models.DO_NOTHING)
-    ingredient = models.ForeignKey(Ingredient, models.DO_NOTHING)
+    품목명 = models.CharField(max_length=100)
+    섭취시주의사항 = models.TextField()
+    업소명 = models.TextField()
+    섭취방법 = models.TextField()
+    주된기능성 = models.TextField()
+    유통기한 = models.TextField()
+    보관방법 = models.TextField()
+    원재료 = models.TextField()
 
     class Meta:
         managed = True
         db_table = 'nutraceuticals'
 
+class Ingredient(models.Model):
+    nutraceuticals_name = models.CharField(max_length=100, primary_key=True)
+    베타카로틴 = models.FloatField(max_length=10)
+    비타민D = models.FloatField(max_length=10)
+    비타민E = models.FloatField(max_length=10)
+    비타민k = models.FloatField(max_length=10)
+    비타민B1 = models.FloatField(max_length=10)
+    비타민B2 = models.FloatField(max_length=10)
+    나이아신 = models.FloatField(max_length=10)
+    비타민B6 = models.FloatField(max_length=10)
+    엽산 = models.FloatField(max_length=10)
+    비타민B12 = models.FloatField(max_length=10)
+    비오틴 = models.FloatField(max_length=10)
+    비타민C = models.FloatField(max_length=10)
+    칼슘 = models.FloatField(max_length=10)
+    마그네슘 = models.FloatField(max_length=10)
+    철 = models.FloatField(max_length=10)
+    아연 = models.FloatField(max_length=10)
+    구리 = models.FloatField(max_length=10)
+    셀레늄_셀렌 = models.FloatField(max_length=10)
+    요오드 = models.FloatField(max_length=10)
+    망간 = models.FloatField(max_length=10)
+    몰리브덴 = models.FloatField(max_length=10)
+    칼륨 = models.FloatField(max_length=10)
+    식이섬유 = models.FloatField(max_length=10)
+    단백질 = models.FloatField(max_length=10)
+    프로바이오틱스 = models.FloatField(max_length=10)
+    진세노사이드 = models.FloatField(max_length=10)
+    루테인 = models.FloatField(max_length=10)
+    EPA_DHA_Sum= models.FloatField(max_length=10)
+    비타민A= models.FloatField(max_length=10)
+
+    class Meta:
+        managed = True
+        db_table = 'ingredient'
 
 class UserReview(models.Model):
     user_review_id = models.BigAutoField(primary_key=True)
     review_rating = models.IntegerField()
     review_contents = models.CharField(max_length=500)
     user = models.ForeignKey(User, models.DO_NOTHING,db_column='member_id')
-    nutraceuticals = models.ForeignKey(Nutraceuticals, models.DO_NOTHING)
 
     class Meta:
         managed = True
@@ -48,7 +73,6 @@ class OnelineReview(models.Model):
     online_review_id = models.BigAutoField(primary_key=True)
     review_rating = models.IntegerField()
     review_contents = models.CharField(max_length=500)
-    nutraceuticals = models.ForeignKey(Nutraceuticals, models.DO_NOTHING)
 
     class Meta:
         managed = True
