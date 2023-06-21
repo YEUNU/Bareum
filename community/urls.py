ab7969874e27bff7dc8309802b6b258d9b243d0c
seungmin1021 <wp050312@gmail.com>
2023-06-21 09:52:56 +0900

    ocr

--- community/urls.py
+++ community/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
 
-    path('list/', views.PostListView.as_view(), name='post-list'),
+    path('list', views.PostListView.as_view(), name='post-list'),
     path('write',views.write_post),
     path('detail/<int:post_id>/',views.post_detail),
-    path('popularlist/', views.popular_posts, name='popular_posts'),
+    path('popularlist', views.popular_posts, name='popular_posts'),
     path('comments/<int:post_id>', views.CommentListView.as_view(), name='comments_list'),
     path('comments/reply/<int:comments_id>',views.CommentReplyListView.as_view(), name='comments_reply_list')
 ]
