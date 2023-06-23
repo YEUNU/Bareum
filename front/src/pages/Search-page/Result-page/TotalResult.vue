2005394bd40fe39ee3d6409121eb89a357b09fd2
mskim <kms97531@gmail.com>
2023-06-23 14:10:06 +0900

    decorate searchoption buttons

--- front/src/pages/Search-page/Result-page/TotalResult.vue
+++ front/src/pages/Search-page/Result-page/TotalResult.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="background bg-whitesmoke" style="margin-top: 59px; min-height: calc(100vh - 59px);">
-        <div style="width: 100%; font-size: 0.8em; font-weight: bold;">
-            <button v-if="route.query.option != null" class="roundbox" style="width: auto; font-size: 1em;"
-            @click="router.push({name: 'resultPage', query: { 'q': route.query.q }})">전체 검색</button>
-            <button v-if="route.query.option != 'name'" class="roundbox" style="width: auto; font-size: 1em;"
-            @click="router.push({name: 'resultPage', query: { 'q': route.query.q, 'option': 'name' }})">이름으로 검색</button>
-            <button v-if="route.query.option != 'personalize'" class="roundbox" style="width: auto; font-size: 1em;"
-            @click="router.push({name: 'resultPage', query: { 'q': route.query.q, 'option': 'personalize' }})">관심분야로 검색</button>
-            <button v-if="route.query.option != 'ingredient'" class="roundbox" style="width: auto; font-size: 1em;"
-            @click="router.push({name: 'resultPage', query: { 'q': route.query.q, 'option': 'ingredient' }})">영양성분으로 검색</button>
+        <div class="search_detail_option_box bg-theme">
+
+            <div id="option_button_container">
+                <button v-if="route.query.option != null"
+                @click="router.push({name: 'resultPage', query: { 'q': route.query.q }})">전체 검색</button>
+                <button v-if="route.query.option != 'name'"
+                @click="router.push({name: 'resultPage', query: { 'q': route.query.q, 'option': 'name' }})">이름으로 검색</button>
+                <button v-if="route.query.option != 'personalize'"
+                @click="router.push({name: 'resultPage', query: { 'q': route.query.q, 'option': 'personalize' }})">관심분야로 검색</button>
+                <button v-if="route.query.option != 'ingredient'"
+                @click="router.push({name: 'resultPage', query: { 'q': route.query.q, 'option': 'ingredient' }})">영양성분으로 검색</button>
+            </div>
             
-            <div v-if="route.query.option">
+            <div v-if="route.query.option" class="bg-white" style="text-align: left; padding: 1vh 4vw; color: #333;">
                 <div>{{ route.query.q }}에 대한 {{ {'name':'이름', 'personalize':'관심분야', 'ingredient':'영양성분'}[route.query.option] }} 검색 결과</div>
             </div>
         </div>
@@ -24,7 +27,7 @@
             <div style="bottom: 0; display: flex; flex-direction: column; text-align: start; background-color: white; margin-bottom: 1vh; padding: 5vw; color: gray;">
                 <span>찾으시는 제품이 없나요?</span>
                 <span style="font-size: 0.9em;">제품 등록을 요청하시면, 금방 추가해드리겠습니다.</span>
-                <router-link to="/">제품등록 요청하러 가기</router-link>
+                <router-link :to="{name: 'mynewaddPage'}">제품등록 요청하러 가기</router-link>
             </div>
         </div>
     </div>
@@ -114,8 +117,32 @@ export default {
 }
 </script>
 
-
 <style>
+
+.search_detail_option_box {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    font-size: 0.8em;
+    font-weight: bold;
+}
+
+#option_button_container {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+
+    position: sticky;
+}
+
+.search_detail_option_box button {
+    flex-grow: 1;
+    border-radius: 10px;
+    margin: 2vh 2vw;
+    font-size: 0.9em;
+    color: #333;
+    background-color: white;
+}
 
 .result_box {
     align-self: center;
