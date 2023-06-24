ed7a135adaaa6e9f399d83b88d65ae35d1c31171
이준혁 <leejunhyeok@ijunhyeog-ui-MacBookPro.local>
2023-06-24 23:18:50 +0900

    addinfo page update

--- front/src/pages/login-page/AddInfo.vue
+++ front/src/pages/login-page/AddInfo.vue
@@ -1,32 +1,37 @@
 <template>
-    <div>
-      로그인 이후 추가정보입력받기
+    <div style="width: 100%; font-weight: bold; font-size:38px;">
+      개인 정보 추가 입력
     </div>
     <form @submit.prevent="submintAddInfo">
-      <div>
-        <label for="nickName">Nickname:</label>
-        <input type="text" id="nickName" name="nickName" v-model="nickName" required/>
+      <div style="text-align: left; margin-top: 20%;">
+        <p style="color: black; font-weight: bold;" >닉네임</p>
+        <input type="text" id="nickName" name="nickName" placeholder="닉네임" v-model="nickName" required style="width:100%; border-radius: 6px; border: 2px solid #eeeeee;"/>
       </div>
-      <div>
-        <label for="birthDay">Birthday:</label>
-        <input type="date" id="birthDay" name="birthDay" v-model="birthDay" required/>
+
+      <div style="text-align: left; margin-top: 10%;">
+        <p style="color: black; font-weight: bold;" >생년월일</p>
+        <input type="date" id="birthDay" name="birthDay" v-model="birthDay" required style="width:100%; border-radius: 6px; border: 2px solid #eeeeee;"/>
       </div>
-      <div>
-        <label for="height">Height:</label>
-        <input type="number" id="height" name="height" v-model="height" required/>
+
+      <div style="text-align: left; margin-top: 10%;">
+        <p style="color: black; font-weight: bold;" >키 (cm)</p>
+        <input type="number" id="height" name="height" placeholder="cm"  v-model="height" required style="width:100%; border-radius: 6px; border: 2px solid #eeeeee;"/>
       </div>
-      <div>
-        <label for="weight">Weight:</label>
-        <input type="number" id="weight" name="weight" pattern="\d*" v-model="weight" required/>
+
+      <div style="text-align: left; margin-top: 10%;">
+        <p style="color: black; font-weight: bold;" >체중 (kg)</p>
+        <input type="number" id="weight" name="weight" placeholder="kg"  pattern="\d*" v-model="weight" required style="width:100%; border-radius: 6px; border: 2px solid #eeeeee;"/>
       </div>
-      <div>
-        <label for="gender">Gender:</label>
-        <input type="radio" id="male" name="gender" value="male" v-model="gender" required/>
-        <label for="male">남자</label>
-        <input type="radio" id="female" name="gender" value="female" v-model="gender" required/>
+
+      <div style="text-align: left; margin-top: 10%;">
+        <p style="color: black; font-weight: bold;" >성별</p>
+        <input type="radio" id="male" name="gender" value="male" v-model="gender" required style="border-radius: 6px; border: 2px solid #eeeeee; margin-right: 2%;"/>
+        <label for="male" style="margin-right: 5%;">남자</label>
+        <input type="radio" id="female" name="gender" value="female" v-model="gender" required style="border-radius: 6px; border: 2px solid #eeeeee; margin-right: 2%;"/>
         <label for="female">여자</label>
       </div>
-      <button>저장</button>
+
+      <button style="width:50%; margin-top: 15%; background-color: #2dce89; border-radius: 5px; color:white; box-shadow: 2px 2px 2px 2px #eeeeee">저장</button>
     </form>
   </template>
   
