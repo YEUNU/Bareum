84204d3da82b86caaf95a88b65e3e7ce1d86856a
dntjr970 <dntjr970@gmail.com>
2023-06-25 18:30:36 +0900

    yaho

--- taking/urls.py
+++ taking/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-from .views import NutraceuticalsSearchAPIView
+from .views import NutraceuticalsSearchAPIView, NutraceuticalsSaveAPIView
+
 app_name = 'api'
 
 urlpatterns = [
-    path('search',NutraceuticalsSearchAPIView.as_view()),
+    path('search', NutraceuticalsSearchAPIView.as_view()),
+    path('save', NutraceuticalsSaveAPIView.as_view()),  # 메서드 지정
 ]
