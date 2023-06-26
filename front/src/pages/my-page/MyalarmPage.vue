b487f3aa36b15a7edcea8b77e4952013c4b2edf7
mskim <kms97531@gmail.com>
2023-06-26 09:56:06 +0900

    fix no module issue

--- front/src/pages/my-page/MyalarmPage.vue
+++ front/src/pages/my-page/MyalarmPage.vue
@@ -3,7 +3,7 @@
 <div class="pushcontainer">
     <nav class="navbar fixed-top bg-white" style="padding: 10px 0;">
         <div class="container-fluid">
-            <span class="navbar-brand" @click="() => {$router.back(); searchbarClear();}" style="margin-right: 0px">
+            <span class="navbar-brand" @click="() => {$router.back();}" style="margin-right: 0px">
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="black" class="bi bi-arrow-left" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"></path>
                 </svg>
