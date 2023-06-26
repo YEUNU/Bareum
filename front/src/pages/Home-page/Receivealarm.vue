4c7f36c40604d8e09bfaf2966a8130c585daf096
junhyeok <natenim@naver.com>
2023-06-26 17:27:44 +0900

    all page update

--- front/src/pages/Home-page/Receivealarm.vue
+++ front/src/pages/Home-page/Receivealarm.vue
@@ -7,11 +7,12 @@
               <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
             </svg>
         </router-link>
-        <router-link to="/" class="navbar-brand">
+        알림센터 
+        <div to="/" class="navbar-brand">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
           <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
           </svg>
-        </router-link>
+        </div>
         </div>
       </nav>
 
