930823e322d9eccff187a149dd95e96af62c563d
NowHyeok <84857521+Now-Hyeok@users.noreply.github.com>
2023-06-27 17:45:11 +0900

    123

--- front/src/pages/Product-page/detail-page/ProductReview.vue
+++ front/src/pages/Product-page/detail-page/ProductReview.vue
@@ -7,6 +7,9 @@
          {{onlineReviews  }}
     </div>
     <div>
+        <div>
+            바름 평균 평점 : {{ averageRating }}
+        </div>
         <div v-for="review in BareumReviews" :key="review.bareum_review_number">
             <img :src="review.user_info.profile_image"/>
             <p>작성자: {{ review.user_info.nickname }}</p>
@@ -33,6 +36,7 @@ export default {
     setup(props){
         const onlineReviews = ref([]);
         const BareumReviews = ref([]);
+        const averageRating = ref(0.0);
 
         const fetchOnlineReview = async(productCode) =>{
             const respnse = await axios.get(`/api/product/online-reviews/${productCode}/`);
@@ -41,6 +45,15 @@ export default {
         const fetchBareumReview = async (productCode) => {
             const respnse = await axios.get(`/api/product/bareum-reviews/${productCode}/`);
             BareumReviews.value = respnse.data
+            let totalRating = 0;
+            let reviewCount = BareumReviews.value.length;
+
+            for (let review of BareumReviews.value) {
+                totalRating += review.rating;
+            }
+        
+            averageRating.value = totalRating / reviewCount;
+
         }
 
         onMounted(() => {
@@ -53,7 +66,7 @@ export default {
             fetchBareumReview,
             onlineReviews,
             BareumReviews,
-
+            averageRating
 
         }
     }
