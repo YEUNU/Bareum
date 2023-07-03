f19599abdf8428b7a933e60cddd18b27a421adec
mskim <kms97531@gmail.com>
2023-07-03 10:03:32 +0900

    change community thumbnail position left to right

--- front/src/pages/Community-page/PopularPost.vue
+++ front/src/pages/Community-page/PopularPost.vue
@@ -3,21 +3,19 @@
   <div class="card" v-for="post in popularPosts" :key="post.id" style="box-shadow: 2px 2px 2px 2px #eeeeee; width: 90vw; margin: 1vh; padding: 2vh;">
     <router-link :to="{ name: 'postDetailContentPage', params: { postId: post.id }}">
       <div class="post_box">
-        <img v-if="post.user.user_profile_img.image !== null" :src="post.user.user_profile_img.image"
-        alt="user profile img" class="img-fluid rounded-circle border border-dark border-3" style="width: 78px; height: auto;">
-        <svg v-else xmlns="http://www.w3.org/2000/svg" width="78" height="78" fill="currentColor" class="bi bi-card-image" viewBox="0 0 16 16">
-          <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-          <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z"/>
-        </svg>
-
         <!-- <img :src="post.image" class="card-img" alt="post image" /> -->
-
         <div class="card-body" style="padding: 1vh 5vw 1vh 3vw; flex-grow: 1;">
           <div class="card-text" style="color: black; font-weight: bold; text-align: start;">{{ post.post_title }}</div>
           <div class="card-text" style="color: black; font-size: 13px; text-align: start;">작성자 : {{ post.user.nickname }}</div>
           <div class="card-text" style="color: black; font-size: 13px; text-align: start;">{{ formatDate(post.post_date) }}</div>
-        <!-- <p class="card-text">내용 : {{ post.post_contents }}</p> -->
+          <!-- <p class="card-text">내용 : {{ post.post_contents }}</p> -->
         </div>
+        <img v-if="post.user.user_profile_img.image !== null" :src="post.user.user_profile_img.image"
+        alt="post img" class="img-fluid border border-dark border-3" style="width: 78px; height: auto; border-radius: 10%;">
+        <svg v-else xmlns="http://www.w3.org/2000/svg" width="78" height="78" fill="currentColor" class="bi bi-card-image" viewBox="0 0 16 16">
+          <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+          <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z"/>
+        </svg>
       </div>
     </router-link>
   </div>
